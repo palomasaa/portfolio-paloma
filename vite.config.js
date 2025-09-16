875369b00c8921698a905b6ab615993de9dd7812
Paloma Sá <palomasaaa@gmail.com>
2025-09-15 21:13:43 -0300

    ci: deploy para GitHub Pages (Vite)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Configuração para dev e GitHub Pages
 export default defineConfig({
+  base: '/portfolio-paloma/', // <- o nome do repositório
   plugins: [react()],
-  base: '/portfolio-paloma/', // nome EXATO do repositório
-  server: { port: 5173, open: true, host: true },
 })
